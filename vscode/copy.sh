40e2c71b0b79b0602a905a7a60f236499da1c0b0
Egor4ik325 <zorinegor325@gmail.com>
2022-03-12 23:49:00 +0300

    Improve vscode conf

--- vscode/copy.sh
+++ vscode/copy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+
 # Copy and override configuration files from VS Code internal location
 
 # Path to VS Code user directory
@@ -7,3 +13,5 @@ VSCODE_USER_PATH="${HOME}/Library/Application Support/Code/User"
 /bin/cp "$VSCODE_USER_PATH"/settings.json .
 /bin/cp "$VSCODE_USER_PATH"/keybindings.json .
 /bin/cp -r "$VSCODE_USER_PATH"/snippets .
+
+echo 'Settings, keybindings and snippets are copied here successfully.'
